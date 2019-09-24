6514377f7e33165869e6b53715506aa80473f47d
linuxwt <tengwanginit@gmail.com>
2019-09-24 16:07:53 +0800

    提交文件

---
+++ snmp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+yum -y install net-snmp net-snmp-devel
+systemctl start snmpd
+systemctl enable snmpd 
+systemctl stop firewalld
+systemctl daemon-reload
+
+>/etc/snmp/snmpd.conf
+cat <<EOF>>  /etc/snmp/snmpd.conf
+com2sec notConfigUser  192.168.0.101      public
+group   notConfigGroup v1           notConfigUser
+group   notConfigGroup v2c           notConfigUser
+view    systemview    included   .1.3.6.1.2.1.1
+view    systemview    included   .1.3.6.1.2.1.25.1.1
+access  notConfigGroup ""      any       noauth    exact  all none none
+view all included .1 80
+syslocation Unknown (edit /etc/snmp/snmpd.conf)
+syscontact Root <root@localhost> (configure /etc/snmp/snmp.local.conf)
+dontLogTCPWrappersConnects yes
+EOF
+
+systemctl restart snmpd
