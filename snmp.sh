3d6c41765dd675c79d0e8cafee7d9032d15dd6fc
linuxwt <tengwanginit@gmail.com>
2019-09-25 10:32:21 +0800

    提交文件

--- snmp.sh
+++ snmp.sh
@@ -6,9 +6,15 @@ systemctl enable snmpd
 systemctl stop firewalld
 systemctl daemon-reload
 
+# 获取cacti服务器的ip
+netcard=$(ls /etc/sysconfig/network-scripts/ | grep ifcfg | grep -v lo)
+card=${netcard//ifcfg-/}
+ip_net=$(ip addr | grep ens33 | grep inet | awk '{print $2}')
+ip=${ip_net//\/24/}
+
 >/etc/snmp/snmpd.conf
 cat <<EOF>>  /etc/snmp/snmpd.conf
-com2sec notConfigUser  192.168.0.101      public
+com2sec notConfigUser  ${ip}     public
 group   notConfigGroup v1           notConfigUser
 group   notConfigGroup v2c           notConfigUser
 view    systemview    included   .1.3.6.1.2.1.1
@@ -20,4 +26,6 @@ syscontact Root <root@localhost> (configure /etc/snmp/snmp.local.conf)
 dontLogTCPWrappersConnects yes
 EOF
 
+yum -y install ntpdate
+ntpdate ${ip}
 systemctl restart snmpd
