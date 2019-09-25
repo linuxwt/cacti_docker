e81d26ab8e6e0c3b7b5970406e9186e1c9761808
linuxwt <tengwanginit@gmail.com>
2019-09-25 11:41:30 +0800

    提交文件

--- client_snmp.sh
+++ client_snmp.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# 获取cacti服务器的ip
+netcard=$(ls /etc/sysconfig/network-scripts/ | grep ifcfg | grep -v lo)
+card=${netcard//ifcfg-/}
+ip_net=$(ip addr | grep ens33 | grep inet | awk '{print $2}')
+ip=${ip_net//\/24/}
+ntpserver="${ip}"
+
 echo -n "please enter the ip and root password of host which you want to monitor->"
-read client_ip password
+read client_ip passwd
 [ -f /usr/bin/expect ] || yum -y install expect
 client_config () {
 /usr/bin/expect << EOF
@@ -26,7 +33,27 @@ EOF
 
 /usr/bin/expect << EOF
 set timeout 200
-spawn ssh ${client_ip} -o StrictHostKeyChecking=no /root/snmp.sh
+spawn ssh ${client_ip} -o StrictHostKeyChecking=no sed -i 's/default/${ip}/g' /etc/snmp/snmpd.conf
+expect "password"
+send "${passwd}\r"
+set timeout 200
+expect eof
+exit
+EOF
+
+/usr/bin/expect << EOF
+set timeout 200
+spawn ssh ${client_ip} -o StrictHostKeyChecking=no ntpdate ${ip}
+expect "password"
+send "${passwd}\r"
+set timeout 200
+expect eof
+exit
+EOF
+
+/usr/bin/expect << EOF
+set timeout 200
+spawn ssh ${client_ip} -o StrictHostKeyChecking=no systemctl restart snmpd
 expect "password"
 send "${passwd}\r"
 set timeout 200
--- snmp.sh
+++ snmp.sh
@@ -6,12 +6,6 @@ systemctl enable snmpd
 systemctl stop firewalld
 systemctl daemon-reload
 
-# 获取cacti服务器的ip
-netcard=$(ls /etc/sysconfig/network-scripts/ | grep ifcfg | grep -v lo)
-card=${netcard//ifcfg-/}
-ip_net=$(ip addr | grep ens33 | grep inet | awk '{print $2}')
-ip=${ip_net//\/24/}
-
 >/etc/snmp/snmpd.conf
 cat <<EOF>>  /etc/snmp/snmpd.conf
 com2sec notConfigUser  ${ip}     public
