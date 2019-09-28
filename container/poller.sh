0f40d95550f550d853dd05762ee6fdb30f8725c8
linuxwt <31132875+linuxwt@users.noreply.github.com>
2019-09-29 04:15:45 +0800

    Update poller.sh

--- container/poller.sh
+++ container/poller.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "*/1 * * * * /usr/bin/docker exec php_cacti /usr/local/php/bin/php /usr/local/Nginx/html/cacti/poller.php --force" >> /etc/crontab    
+/usr/bin/docker exec php_cacti /usr/local/php/bin/php /usr/local/Nginx/html/cacti/poller.php --force" >> /etc/crontab    
 systemctl restart crond
 
