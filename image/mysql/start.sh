bd61437f2774cc4647359ee3d2a260d2f501c2db
linuxwt <tengwanginit@gmail.com>
2019-09-29 02:48:04 +0800

    提交文件

--- image/mysql/start.sh
+++ image/mysql/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mysqld --initialize-insecure --user=mysql > /dev/null &&
 mysqld  &
-sleep 10
+sleep 30
 mysql < /tmp/config.sql
-sleep 10
+sleep 30
 ps -wef | grep mysql | grep -v grep | awk '{print $2}' | xargs kill -9
 mysqld
